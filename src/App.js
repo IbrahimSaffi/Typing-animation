4bc26c5449ecfb35f2f4e9afea5fd17d02d0cbf8
Ibrahim Saffi <ibrahimsaffinew@gmail.com>
2022-08-06 05:32:06 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import Icon from "./Icon.jsx"
 function App() {
+  let intro = "Hello, World".split("")
+  let [text,setText] = useState("")
+  useEffect(()=>{
+    setTimeout(()=>{
+      let tempText = text
+      console.log(tempText)
+       if(tempText.length!==intro.length){
+        tempText +=intro[tempText.length]
+         setText(tempText)
+       }
+     },500)
+    },[text])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='button'>
+        <span className='wave' >👋
+        </span>
+      <h3>
+       
+          Get in touch
+          
+      </h3>
+
+      </div>
+     <h1>{text} <span className='typing' >|</span> </h1> 
+      <h3 className='intro'>
+      I'm Ibrahim
+      </h3>
+      <div className='icons'>
+        {Icon({iconName:"twitter",className:"icon"})}
+        {Icon({iconName:"linkedin",className:"icon"})}
+        {Icon({iconName:"github",className:"icon"})}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
